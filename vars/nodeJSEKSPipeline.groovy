8798ec73161727e1eb8057c70dea0a73bfa61a8f
Sivakumar Reddy M <info@joindevops.com>
2026-04-30 12:11:56 +0530

    oil and karam added

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -1,23 +1,21 @@
-def call (){
-    def call (Map configMap){
-        pipeline {
-            agent { node { label 'roboshop' } } 
-            stages {
-                stage('test') {
-                    steps {
-                        sh """
-                            echo "triggering test: ${configMap.project}"
-                        """
-                    }
+def call(Map configMap){
+    pipeline {
+        agent { node { label 'roboshop' } } 
+        stages {
+            stage('test') {
+                steps {
+                    sh """
+                        echo "triggering test: ${configMap.project}"
+                    """
                 }
             }
-            post {
-                success {
-                    echo "Pipeline succeeded on branch: ${env.BRANCH_NAME}"
-                }
-                failure {
-                    echo "Pipeline failed on branch: ${env.BRANCH_NAME}"
-                }
+        }
+        post {
+            success {
+                echo "Pipeline succeeded on branch: ${env.BRANCH_NAME}"
+            }
+            failure {
+                echo "Pipeline failed on branch: ${env.BRANCH_NAME}"
             }
         }
     }
